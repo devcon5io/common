b5bcfedd6fd90e23ff8314a95827de6c6c291524
Gerald Mücke <gerald.muecke@devcon5.io>
2016-01-12 11:44:41 +0100

    added classutils

---
+++ classutils/src/main/java/io/devcon5/classutils/ClassStreams.java
@@ -0,0 +1,46 @@
+package io.devcon5.classutils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Utility class for dealing with classes and streams Created by m4g on 11.01.2016.
+ */
+public final class ClassStreams {
+
+    private ClassStreams() {
+    }
+
+    /**
+     * Creates a stream of classes representing the supertypes (excluding Object.class) of the specified type.
+     *
+     * @param type
+     *         the type whose supertypes should be returned as a stream
+     *
+     * @return stream of the supertypes of the specified class
+     */
+    public static Stream<Class> supertypes(final Class type) {
+        return selfAndSupertypes(type.getSuperclass());
+    }
+
+    /**
+     * Creates a stream of classes containing the class itself and all its the supertypes (excluding Object.class) of
+     * the specified type.
+     *
+     * @param type
+     *         the type whose supertypes should be returned as a stream
+     *
+     * @return stream of the class itself and its supertypes
+     */
+    public static Stream<Class> selfAndSupertypes(final Class type) {
+        Class current = type;
+        final List<Class> result = new ArrayList<>();
+        do {
+            result.add(current);
+            current = current.getSuperclass();
+        } while (current != Object.class);
+        return result.stream();
+    }
+
+}
---
+++ classutils/src/test/java/io/devcon5/classutils/ClassStreamsTest.java
@@ -0,0 +1,53 @@
+package io.devcon5.classutils;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.Test;
+
+/**
+ * Created by m4g on 11.01.2016.
+ */
+public class ClassStreamsTest {
+
+    @Test
+    public void testSupertypes() throws Exception {
+
+        //arrange
+
+        //act
+        List<Class> superTypes = ClassStreams.supertypes(Leaf.class).collect(Collectors.toList());
+
+        //assert
+        assertThat(superTypes, hasSize(2));
+        assertThat(superTypes, contains(Intermediate.class, BaseType.class));
+    }
+
+    @Test
+    public void testSelfAndSupertypes() throws Exception {
+
+        //arrange
+
+        //act
+        List<Class> superTypes = ClassStreams.selfAndSupertypes(Leaf.class).collect(Collectors.toList());
+
+        //assert
+        assertThat(superTypes, hasSize(3));
+        assertThat(superTypes, contains(Leaf.class, Intermediate.class, BaseType.class));
+    }
+
+    static class BaseType {
+
+    }
+
+    static class Intermediate extends BaseType {
+
+    }
+
+    static class Leaf extends Intermediate {
+
+    }
+}
