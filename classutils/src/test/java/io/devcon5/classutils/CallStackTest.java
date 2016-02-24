3b5e3bbccc4e947ce97be8501b1732d88b491211
Gerald Mücke <gerald.muecke@devcon5.io>
2016-02-24 20:23:14 +0100

    - added resource resolver - added callstack util

---
+++ classutils/src/test/java/io/devcon5/classutils/CallStackTest.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2015-2016 DevCon5 GmbH, info@devcon5.ch
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.devcon5.classutils;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Created by Gerald Muecke on 26.11.2015.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class CallStackTest {
+
+    @Mock
+    private ClassLoader classLoader;
+    private ClassLoader origCtxCl;
+
+    @Before
+    public void setUp() throws Exception {
+        this.origCtxCl = Thread.currentThread().getContextClassLoader();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Thread.currentThread().setContextClassLoader(origCtxCl);
+    }
+
+    @Test
+    public void testGetCallerClass_defaultClassLoader() throws Exception {
+
+        //prepare
+
+        //act
+        Class<?> cls = new CallingClass().callingMethod();
+
+        //assert
+        assertEquals(CallStackTest.class, cls);
+    }
+
+
+    @Test
+    public void testGetCallerClass_contextClassLoader() throws Exception {
+
+        //prepare
+        when(classLoader.loadClass(CallStackTest.class.getName())).thenReturn((Class)MockClass.class);
+        Thread.currentThread().setContextClassLoader(classLoader);
+
+        //act
+        Class<?> cls = new CallingClass().callingMethod();
+
+        //assert
+        assertEquals(MockClass.class, cls);
+    }
+
+    @Test
+    public void testCurrentMethodWithoutTracking() throws Exception {
+        //prepare
+
+        //act
+        assertEquals(Optional.empty(), CallStack.currentMethod());
+        //assert
+
+    }
+
+    @Test
+    public void testTrackAndGetCurrentMethod() throws Exception {
+        //prepare
+        MethodTrackTarget target = CallStack.track(new MethodTrackTarget());
+
+        //act
+        String result = target.getString();
+
+        //assert
+        assertEquals("getString", result);
+
+    }
+
+    /**
+     * Placeholder for a class that wants to get the caller of the calling method.
+     */
+    public static class CallingClass {
+        private Class<?> callingMethod() {
+
+            return CallStack.getCallerClass();
+        }
+    }
+
+    public static class MockClass extends CallStackTest {
+
+    }
+
+    public static class MethodTrackTarget {
+
+        public String getString(){
+            return CallStack.currentMethod().get().getName();
+        }
+
+    }
+
+
+}
