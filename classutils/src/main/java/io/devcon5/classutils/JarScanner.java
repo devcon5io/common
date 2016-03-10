bfc5d1ef3f3fe4350922ccb8ec389472e876dd39
Gerald Mücke <gerald.muecke@devcon5.io>
2016-03-10 14:15:16 +0100

    DCCOM-3 added JarScanner

---
+++ classutils/src/main/java/io/devcon5/classutils/JarScanner.java
@@ -0,0 +1,198 @@
+package io.devcon5.classutils;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * A scanner for contents of jar files. The scanner supports scanning for packages and contents.
+ */
+public class JarScanner {
+
+    /**
+     * Set of entry-prefixes to be excluded from scanning
+     */
+    private final Set<String> ignore = new HashSet<>();
+
+    /**
+     * Set of Jar locators to be scanned
+     */
+    private final Set<URL> jars = new HashSet<>();
+
+    /**
+     * env map for opening Zip-Filesystems in read-only mode
+     */
+    private static final Map<String, String> READY_ONLY_ENV;
+
+    static {
+        final Map<String, String> env = new HashMap<>();
+        env.put("create", "false");
+        READY_ONLY_ENV = Collections.unmodifiableMap(env);
+    }
+
+    /**
+     * Excludes the specified folders from scanning. The ignore pattern checks matches packages and classes
+     * starting with the specified string.
+     *
+     * @param folders
+     *         folders to be ignored
+     *
+     * @return this scanner
+     */
+    public JarScanner ignore(Collection<String> folders) {
+        this.ignore.addAll(folders);
+        return this;
+    }
+
+    /**
+     * Excludes the specified folders from scanning. The ignore pattern checks matches packages and classes
+     * starting with the specified string.
+     *
+     * @param folder
+     *         folder(s) to be ignored
+     *
+     * @return this scanner
+     */
+    public JarScanner ignore(String... folder) {
+        return ignore(Arrays.asList(folder));
+    }
+
+    /**
+     * Adds a jar file to be scanned
+     *
+     * @param jar
+     *         jar(s) to be added to the scanner
+     *
+     * @return this scanner
+     */
+    public JarScanner addJar(URL... jar) {
+        return addJar(Arrays.asList(jar));
+    }
+
+    /**
+     * Adds a jar file to be scanned
+     *
+     * @param jars
+     *         jars to be added to the scanner
+     *
+     * @return this scanner
+     */
+    public JarScanner addJar(Collection<URL> jars) {
+        this.jars.addAll(jars);
+        return this;
+    }
+
+    /**
+     * Scans the specified jars for packages and returns a list of all packages found in that jar. The scanner makes no
+     * distinction between packages and folders.
+     *
+     * @return a collection of all package names found in the jar
+     */
+    public Collection<String> scanPackages() {
+        return scanJar(p -> Files.isDirectory(p) && !isIgnored(p.toAbsolutePath()));
+    }
+
+    /**
+     * Scans the specified jars for packages and returns a list of all packages found in that jar. The scanner makes no
+     * distinction between packages and folders.
+     *
+     * @return a collection of all classes names found in the jar
+     */
+    public Collection<String> scanClasses() {
+        return scanJar(p -> Files.isRegularFile(p) && p.toString().endsWith(".class") && !isIgnored(p.toAbsolutePath()));
+    }
+
+    /**
+     * Scans the jars of the JarScanner finding all items matching the path filter
+     * @param pathFilter
+     *  the filter to find items in the jars
+     * @return
+     *  Collections of items of the jars matching the filter
+     */
+    private Collection<String> scanJar(Predicate<Path> pathFilter) {
+        return jars.parallelStream()
+                   .map(JarScanner::createJarUri)
+                   .flatMap(u -> scanJar(u, pathFilter).stream())
+                   .distinct()
+                   .collect(Collectors.toList());
+    }
+
+    /**
+     * Scans the jar and filters all elements
+     *
+     * @param u
+     *         the uri of the jar file to be scanned
+     * @param pathPredicate
+     *         the matching predicate for collecting the entries
+     *
+     * @return a stream of collected strings
+     */
+    private Collection<String> scanJar(URI u, Predicate<Path> pathPredicate) {
+        try (FileSystem fs = FileSystems.newFileSystem(u, READY_ONLY_ENV)) {
+            return Files.walk(fs.getPath("/"))
+                        .filter(pathPredicate)
+                        .map(f -> toFQName(f.toAbsolutePath()))
+                        .distinct()
+                        .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Converts the path to a fully qualified java entity (package or class)
+     *
+     * @param file
+     *         the file to be converted
+     *
+     * @return a string representing the fully qualified package or classname
+     */
+    private String toFQName(Path file) {
+        return file.toString()
+                   .replaceAll("(\\/|\\\\)", ".")
+                   .replaceAll("(^\\.+|(\\.+|\\.class)$)", "");
+    }
+
+    /**
+     * Checks if the specified path is on the ignore list
+     *
+     * @param p
+     *         the path to check
+     *
+     * @return true if the path should be ignored
+     */
+    private boolean isIgnored(Path p) {
+        final String path = toFQName(p);
+        return this.ignore.stream().anyMatch(path::startsWith);
+    }
+
+    /**
+     * Creates a jar-protocol uri for the specified url
+     *
+     * @param u
+     *         the url to be accessed as jar file
+     *
+     * @return the URI for the jar file
+     */
+    private static URI createJarUri(URL u) {
+        try {
+            return URI.create("jar:" + u.toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ classutils/src/test/java/io/devcon5/classutils/JarScannerTest.java
@@ -0,0 +1,118 @@
+package io.devcon5.classutils;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Test;
+
+/**
+ *
+ */
+public class JarScannerTest {
+
+    /**
+     * The class under test
+     */
+    private JarScanner subject = new JarScanner();
+
+    @Test
+    public void testIgnore_list() throws Exception {
+        //prepare
+        URL jar1Url = JarScannerTest.class.getResource("test1.jar");
+        URL jar2Url = JarScannerTest.class.getResource("test2.jar");
+        subject.addJar(Arrays.asList(jar1Url, jar2Url));
+
+        //act
+        subject.ignore(Arrays.asList("META-INF", "io.devcon5.mixin"));
+
+        //assert
+        Collection<String> pkgs = subject.scanPackages();
+        assertTrue(pkgs.contains("io.devcon5.classutils"));
+        assertFalse(pkgs.contains("io.devcon5.mixin"));
+        assertFalse(pkgs.contains("META-INF"));
+    }
+
+    @Test
+    public void testIgnore_varargs() throws Exception {
+        //prepare
+        URL jar1Url = JarScannerTest.class.getResource("test1.jar");
+        URL jar2Url = JarScannerTest.class.getResource("test2.jar");
+        subject.addJar(Arrays.asList(jar1Url, jar2Url));
+
+        //act
+        subject.ignore("META-INF", "io.devcon5.mixin");
+
+        //assert
+        Collection<String> pkgs = subject.scanPackages();
+        System.out.println(pkgs);
+        assertTrue(pkgs.contains("io.devcon5.classutils"));
+        assertFalse(pkgs.contains("io.devcon5.mixin"));
+        assertFalse(pkgs.contains("META-INF"));
+    }
+
+    @Test
+    public void testAddJar() throws Exception {
+        //prepare
+        URL jar1Url = JarScannerTest.class.getResource("test1.jar");
+        URL jar2Url = JarScannerTest.class.getResource("test2.jar");
+
+        //act
+        subject.addJar(Arrays.asList(jar1Url, jar2Url));
+
+        //assert
+        Collection<String> pkgs = subject.scanPackages();
+        assertTrue(pkgs.contains("io.devcon5.classutils"));
+        assertTrue(pkgs.contains("io.devcon5.mixin"));
+        assertTrue(pkgs.contains("META-INF"));
+    }
+
+    @Test
+    public void testAddJar_varargs() throws Exception {
+        //prepare
+        URL jar1Url = JarScannerTest.class.getResource("test1.jar");
+        URL jar2Url = JarScannerTest.class.getResource("test2.jar");
+
+        //act
+        subject.addJar(jar1Url, jar2Url);
+
+        //assert
+        Collection<String> pkgs = subject.scanPackages();
+        assertTrue(pkgs.contains("io.devcon5.classutils"));
+        assertTrue(pkgs.contains("io.devcon5.mixin"));
+        assertTrue(pkgs.contains("META-INF"));
+    }
+
+    @Test
+    public void testScanPackages() throws Exception {
+        //prepare
+        URL jarUrl = JarScannerTest.class.getResource("test1.jar");
+        subject.addJar(jarUrl);
+
+        //act
+        Collection<String> pkgs = subject.scanPackages();
+
+        //assert
+        assertTrue(pkgs.contains("io"));
+        assertTrue(pkgs.contains("io.devcon5"));
+        assertTrue(pkgs.contains("io.devcon5.classutils"));
+    }
+
+    @Test
+    public void testScanClasses() throws Exception {
+        //prepare
+        URL jarUrl = JarScannerTest.class.getResource("test1.jar");
+        subject.addJar(jarUrl);
+
+        //act
+        Collection<String> classes = subject.scanClasses();
+
+        //assert
+        assertTrue(classes.contains("io.devcon5.classutils.ClassStreams"));
+        assertFalse(classes.contains("io"));
+        assertFalse(classes.contains("io.devcon5"));
+        assertFalse(classes.contains("io.devcon5.classutils"));
+    }
+}
